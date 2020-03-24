a50e8b2ba7c02078e79bfba5b07b09420d371b76
ChrisAB <cristianbatrin@gmail.com>
2020-03-24 20:03:08 +0200

    Changed writeFile to remove errors

--- writeFile.c
+++ writeFile.c
@@ -56,7 +56,7 @@ int verifyIntegrity(char *filePath, int pieceLength, int pieceIndex, char* initi
 	unsigned char hash[SHA_DIGEST_LENGTH];
 	char *bytes = readFromFile(filePath, pieceLength, pieceIndex);
 
-	if (initialSHA == SHA1(bytes, sizeof(bytes), hash){
+	if (initialSHA == SHA1(bytes, sizeof(bytes), hash)){
 		return 1;
 	}
 	return 0;
